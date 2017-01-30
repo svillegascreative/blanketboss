da48939be7cafa4a08dc9879ac7ad9e47f034955
sylvia villegas <sylvia@svillegas.com>
2017-01-30 14:21:32 -0500

    Create Pages controller, routes & views for index, about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "blankets#index"
+  root "pages#index"
 
   resources :blankets
 
+  # pages
+  get "/about" => "pages#about"
+
+  # blankets
   get "/blankets" => "blankets#list"
 
 end
