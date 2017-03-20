39087d50647ffabbd8f64097cd580c9789ccc586
sylvia villegas <sylvia@svillegas.com>
2017-03-19 22:15:17 -0400

    Change homepage from "index" to "home"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "pages#index"
+  root "pages#home"
 
   # pages
   get "/about" => "pages#about", as: "about"
