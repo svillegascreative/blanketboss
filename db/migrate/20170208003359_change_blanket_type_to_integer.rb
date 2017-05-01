4ed4ed20a68eaecec50f639d4e74be0331171f7d
sylvia villegas <sylvia@svillegas.com>
2017-05-01 12:45:53 -0400

    Cancel old table change preventing heroku migration

--- db/migrate/20170208003359_change_blanket_type_to_integer.rb
+++ db/migrate/20170208003359_change_blanket_type_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeBlanketTypeToInteger < ActiveRecord::Migration[5.0]
   def change
-    change_column :blankets, :blanket_type, :integer
+    # change_column :blankets, :blanket_type, :integer
   end
 end
