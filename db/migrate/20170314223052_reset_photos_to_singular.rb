4f04c997a06f8e1e14e00dee852d020f4b395364
sylvia villegas <sylvia@svillegas.com>
2017-05-01 13:02:59 -0400

    Cancel another migration ...?

--- db/migrate/20170314223052_reset_photos_to_singular.rb
+++ db/migrate/20170314223052_reset_photos_to_singular.rb
@@ -1,6 +1,6 @@
 class ResetPhotosToSingular < ActiveRecord::Migration[5.0]
   def change
-    remove_column :blankets, :photos, :json
-    add_column :blankets, :photo, :string
+    # remove_column :blankets, :photos, :json
+    # add_column :blankets, :photo, :string
   end
 end
