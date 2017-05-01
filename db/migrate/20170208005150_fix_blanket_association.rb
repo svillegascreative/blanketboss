5c818f06f950771b0a0c39b88e0728bf48056349
sylvia villegas <sylvia@svillegas.com>
2017-05-01 12:56:16 -0400

    Cancel another migration

--- db/migrate/20170208005150_fix_blanket_association.rb
+++ db/migrate/20170208005150_fix_blanket_association.rb
@@ -1,8 +1,8 @@
 class FixBlanketAssociation < ActiveRecord::Migration[5.0]
   def change
     change_table :blanket_types do |t|
-      t.remove :blanket_id
-      t.remove :index_blanket_types_on_blanket_id
+      # t.remove :blanket_id
+      # t.remove :index_blanket_types_on_blanket_id
     end
 
     change_table :blankets do |t|
