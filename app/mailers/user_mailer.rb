3c97859a445eb69b9d9635e4c6bb432e19ea9b94
sylvia villegas <sylvia@svillegas.com>
2017-05-04 18:45:49 -0400

    Configure user mailer to send password resets

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,12 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: user.email, subject: "Thanks for joining Blanket Boss!")
   end
+
+  def reset_password_email(user)
+    @user = User.find(user.id)
+    @url  = edit_password_reset_url(@user.reset_password_token)
+    mail(:to => user.email,
+         :subject => "Your Blanket Boss password has been reset")
+  end
+
 end
