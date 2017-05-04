1b96976d0e8accd7b28af59493852bdaa8410a70
sylvia villegas <sylvia@svillegas.com>
2017-05-03 23:33:53 -0400

    Ensure header doesn't cover any content

---
+++ app/assets/javascripts/general.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  var headerHeight = $('.app-header').height();
+
+  $('main').css('padding-top', headerHeight * 2);
+  $('#home-top').css('padding-top', headerHeight * 2);
+
+});
