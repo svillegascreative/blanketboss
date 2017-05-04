0d7cf9d736f80e79491b36d977f9f87b992e66ac
sylvia villegas <sylvia@svillegas.com>
2017-05-04 17:14:41 -0400

    Install password reset submodule

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408142440) do
+ActiveRecord::Schema.define(version: 20170504211017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,23 +59,27 @@ ActiveRecord::Schema.define(version: 20170408142440) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                    null: false
+    t.string   "email",                                       null: false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.integer  "failed_logins_count",          default: 0
+    t.integer  "failed_logins_count",             default: 0
     t.datetime "lock_expires_at"
     t.string   "unlock_token"
     t.datetime "last_login_at"
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
     t.string   "last_login_from_ip_address"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
     t.index ["unlock_token"], name: "index_users_on_unlock_token", using: :btree
   end
 
