fa2c3cf1e6bac52ca68580c6d45b283080088f8d
sylvia villegas <sylvia@svillegas.com>
2017-05-04 22:54:23 -0400

    Animate flash messages

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
 
+  // push content down so it is not hidden under header
   var headerHeight = $('.app-header').height();
 
   $('main').css('padding-top', headerHeight * 2);
   $('#home-top').css('padding-top', headerHeight * 2);
+
+
+  // dismiss flash messages
+  $('.flash i').on('click', function(e) {
+    $('.flash').slideUp(200);
+  });
+  // $('.flash').delay(3000).slideUp(1000);
 
 });
