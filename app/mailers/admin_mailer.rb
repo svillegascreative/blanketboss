5bedc58d5bc9b457454c52e02cb1e314a4020d7c
sylvia villegas <sylvia@svillegas.com>
2017-05-26 00:30:36 -0400

    Create contact form mailer action

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,12 @@
 class AdminMailer < ApplicationMailer
+  default from: 'blanketboss@svillegas.com'
+
+  def contact_form(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+
+    mail(to: 'blanketboss@svillegas.com', subject: 'Feedback from Blanket Boss Contact Form')
+  end
+
 end
