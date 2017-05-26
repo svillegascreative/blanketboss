a3bf541c774b75901a7915400a0258c69d16a448
sylvia villegas <sylvia@svillegas.com>
2017-05-25 23:43:53 -0400

    Set welcome email to deliver_later

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user.save
       auto_login(@user, should_remember = false)
       redirect_to blankets_path, success: "Registration successful!"
-      UserMailer.welcome_email(@user).deliver_now
+      UserMailer.welcome_email(@user).deliver_later
     else
       flash.now[:error] = "Hmm... Registration failed for some reason. Please try again."
       render :new
