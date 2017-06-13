9b859f964306471a55b7e7488247be9e9ec2eff9
sylvia villegas <sylvia@svillegas.com>
2017-06-13 18:43:32 -0400

    Put filter/download/email options in tabs

--- app/assets/javascripts/blankets.js
+++ app/assets/javascripts/blankets.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
+  // Blanket list action tabs
+  $('.accordion-tabs').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+
+
   // Blanket filters
   $('.filter-select').SumoSelect();
   $('.filter-select-multiple').SumoSelect({placeholder: 'All'});
