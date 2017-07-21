570a2b446a57b0d58f382c91c1a5a7f1fc519154
sylvia villegas <sylvia@svillegas.com>
2017-07-21 13:36:24 -0400

    Remove unused code

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
 
-  var mouseX, mouseY;
-  var ww = $( window ).width();
-  var wh = $( window ).height();
-  var traX, traY;
-  $(document).mousemove(function(e){
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    traX = ((mouseX) / 50) ;
-    traY = ((mouseY) / 10);
-    $(".blanket-logo").css({"background-position": traX + "%" + traY + "%"});
-  });
-
 });
